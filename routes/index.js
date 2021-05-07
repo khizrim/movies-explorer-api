1ef8e07e745ac71b61fdb73124faaf8eafe3aa2f
khizrim <kh.makhmudov@me.com>
2021-05-07 10:12:19 +0300

    fix: remove cookie validation from main routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 
 const { auth } = require('../middlewares/auth.js');
-const { validateCookies } = require('../middlewares/validators.js');
 
 router.use('/', require('./access'));
 
-router.use('/users', validateCookies, auth, require('./users'));
-router.use('/movies', validateCookies, auth, require('./movies'));
-router.use('*', validateCookies, auth, require('./404'));
+router.use('/users', auth, require('./users'));
+router.use('/movies', auth, require('./movies'));
+router.use('*', auth, require('./404'));
 
 module.exports = router;
