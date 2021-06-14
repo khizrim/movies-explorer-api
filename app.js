86c453d3fb21c1d4462e77112e04505da59fcd40
Khizri Makhmudov <kh.makhmudov@me.com>
2021-06-14 19:32:16 +0300

    fix: correct server adress

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: 'https://movies-explorer.khizrim.ru',
   credentials: true,
 }));
 app.use(express.json());
